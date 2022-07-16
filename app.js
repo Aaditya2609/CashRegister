7579ed926b29807250e46d05cb6919fc11e68699
Aaditya Khantal <khantalak2609@gmail.com>
2022-07-16 22:44:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-var inputBillAmt=document.querySelector("#bill-amt");
+var inputBillAmt= document.querySelector("#bill-amt");
 var inputCashRecieved=document.querySelector("#cash-recieved");
 var error=document.querySelector("#errorMsg");
 var button=document.querySelector("#button");
 
 function clickEventHandler()
 {
+    var bilamt=inputBillAmt.value;
+    var cash=inputCashRecieved.value
+    console.log(inputBillAmt.value)
+    console.log(inputCashRecieved.value)
     if(inputBillAmt.value>0)
     {
-        if(inputCashRecieved.value>=inputBillAmt.value)
+        console.log(inputBillAmt.value)
+        console.log(inputCashRecieved.value)
+        if(cash>bilamt)
         {
+            console.log(inputBillAmt.value)
+            console.log(inputCashRecieved.value)
           var change=inputCashRecieved.value-inputBillAmt.value;
-          console.log(change);  
+          console.log(change); 
+          showError("WELLDONE")
         }
-        else showError("false")
+        else showError("Cash recieved less than bill amount, ")
     }
     else showError("Bill amount should be greater than 0")
 }
